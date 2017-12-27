9a218bbdad4362706d235cf6d76e6694eaddbf55
jeevaka <jeevaka@empite.com>
2017-12-27 09:27:49 +0530

    first commit

---
+++ sample/src/reducers/sampleReducer.js
@@ -0,0 +1,16 @@
+
+
+const INITIAL_STATE = {
+  data: null,
+  error: "",
+
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+
+      default:
+      return state;
+    }
+
+};
